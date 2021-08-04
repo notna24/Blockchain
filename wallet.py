ffa183d50294e6673afa4e7c121999022e1683e8
hello512 <anton.anderten@me.com>
2021-08-05 00:29:05 +0200

    worked on key related stuff

--- wallet.py
+++ wallet.py
@@ -1,13 +1,14 @@
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
-from mempool import Mempool
+from mempool import MemPool
 
 import os
 
 class Wallet:
     def __init__(self):
-        self.keys = []
+        self.keys= []
 
 
     def make_transaction(self):
@@ -25,13 +26,29 @@ class Wallet:
 
 ## ALL KEY RELATED STUFF FOLLOWS
 
-def gen_key():
-    pass
+def gen_key_pair():
+    private_key = gen_private_key()
+    return (private_key, private_key.public_key())
 
 
-def serealize_public_key(password=None):
-    pass
+def gen_private_key():
+    return rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
 
 
-def serealize_private_key(password=None):
-    pass
+
+def serealize_public_key(public_key, password=None):
+    public_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+
+def serealize_private_key(private_key, password=None):
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCSB,
+        encryption_algorithm=serialization.BestAvailableEncryption(password) if password != None else serialization.NoEncryption()
+    )
