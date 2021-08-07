d5fe1347bd91572346c9f5399e29ee4ef7c2f497
hello512 <anton.anderten@me.com>
2021-08-07 17:18:55 +0200

    can now be stored in json format and checks if incoming transactions are verifyed

--- mempool.py
+++ mempool.py
@@ -1,4 +1,6 @@
+from transaction import Transaction
 
+import json
 
 
 
@@ -7,13 +9,32 @@ class MemPool:
         self.transactions = []
 
     def add_transaction(self, transaction):
-        return self.transactions.append(transaction)
+        assert isinstance(transaction, Transaction)
+        if transaction.verify():
+            return self.transactions.append(transaction)
+        else:
+            return false
 
     def rm_transaction(self, transaction):
+        assert isinstance(transaction, Transaction)
         if transaction in self.transactions:
             return self.transactions.remove(transaction)
         else:
             return False
 
+    def get_dict(self):
+        return {
+                "transactions": self.transactions
+        }
+
     def get_json(self):
-        pass
+        return json.dumps(self.get_dict())
+
+    def save_to_file(self, file_name="mempool.json"):
+        with open(file_name, "w") as file:
+            file.write(self.get_json())
+
+
+if __name__ == "__main__":
+    mem = MemPool()
+    mem.save_to_file()
