5aa217acdad7a4138f04244bef3bee8690ef0f3a
hello512 <anton.anderten@me.com>
2021-08-18 15:06:12 +0200

    trying to fix input

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from block import Block
 from blockchain import Blockchain
-from transaction import Transaction, CoinbaseTransaction, Utxo
-from wallet import Wallet
 from mempool import MemPool
 
 print("Blockchain started...")
