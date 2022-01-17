4a23ed92be4e33b5a4e54e159af242ada70d3d47
hello512 <anton.anderten@me.com>
2022-01-17 22:19:53 +0100

    tryed to add a header, but I will finish it later

--- block.py
+++ block.py
@@ -2,20 +2,46 @@ from cryptography.hazmat.primitives import hashes
 
 import json
 
-from transaction import Transaction
+from transaction import Transaction, CoinbaseTransaction
 
 ## this uses Proof of work method
-## but I don't really like POW method
+
+
+# I wanted to use a header with a merkel root, but it's too much effort for me rn. I might add it later.
+
+
+class Header:
+	# not used jet
+	def __init__(self, pv_hash : bytes = b"", nonce : int = 0, merkle_root : bytes = b""):
+		self.pv_hash = pv_hash
+		self.nonce = nonce
+		self.merklde_root = merkle_root
+
+	def hash(self):
+		pass
+
+
+	def get_dict(self):
+		return {
+			"pv_hash": self.pv_hash,
+			"nonce": self.nonce,
+			"merkle_root": self.merklde_root
+		}
+
+	def get_json(self):
+		return json.dumps(self.get_dict())
 
 class Block:
-	def __init__(self, header, nonce, pv_hash):
-		self.pv_hash = pv_hash
-		self.header = header
+	def __init__(self):
+		self.header = Header()
 		self.timestamp = 0
 		self.transactions = []
-		self.nonce = nonce
 		self.difficulty = 0
+		## should later be in the header
+		self.pv_hash = b""
+		self.nonce = 0
 		self.hash = b""
+
 
 	def get_dict(self):
 		return {
@@ -37,8 +63,12 @@ class Block:
 			composed += transaction.calc_hash()
 		composed += bytes(self.nonce)
 		digest.update(composed)
+		self.hash = digest.finalize()
 		return digest.finalize()
 
 	def add_transaction(self, transaction):
-		assert isinstance(transaction, Transaction)
+		#assert isinstance(transaction, Transaction)
 		self.transactions.append(transaction)
+
+ORIGIN_BLOCK = Block()
+ORIGIN_BLOCK.add_transaction(CoinbaseTransaction(b"", 25))
