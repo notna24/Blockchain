aef1c8ddef2d7a7519d6c16c495ebc8c15a5319c
hello512 <anton.anderten@me.com>
2022-01-29 12:30:06 +0100

    removed obsolete comments

--- main.py
+++ main.py
@@ -26,10 +26,7 @@ if __name__ == "__main__":
 	with open("keys/origin_key.json", "r") as key_file:
 		json_keys = key_file.read()
 	okj = json.loads(json_keys)
-	#print(okj.get("public_key"))
-	#print(wallet.load_public_pem_key(okj.get("public_key").encode("utf-8")))
 	OW.keys.append((okj.get("public_key").encode("utf-8"), okj.get("private_key").encode("utf-8")))
-	#OW.key_dict[okj.get("public_key").encode("utf-8")] =  okj.get("private_key").encode("utf-8")
 
 	W1 = wallet.Wallet()
 	W1.gen_keys()
@@ -37,13 +34,9 @@ if __name__ == "__main__":
 	W2 = wallet.Wallet()
 	W2.gen_keys()
 	
-	#print(OW.key_dict)
 
 	OW.make_auto_transaction([W1.keys[0][0]], [2])
-	#print(OW.get_dict())
 	bb = BC.blocks
-	#print(MP.get_dict())
-	#print(wallet.MEMPOOL.get_dict())
 	
 	MI.pub_key = W1.keys[0][0]
 	MI.start()
